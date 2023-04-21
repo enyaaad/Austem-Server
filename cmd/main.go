7ecf787092b338cc29b4fe6a634a51e48a996026
enyaaad <enindima1@gmail.com>
2023-04-21 18:24:51 +0700

    [update] auth and tryin to make cors

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,57 @@
 package main
 
+import (
+	"fmt"
+	"github.com/rs/cors"
+	"net/http"
+)
+
+var users = map[string]string{
+	"asd": "123",
+}
+
+func isAuthorised(username, password string) bool {
+	pass, ok := users[username]
+	if !ok {
+		return false
+	}
+
+	return password == pass
+}
+
+func greeting(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Authorization")
+	username, password, ok := r.BasicAuth()
+	if !ok {
+		w.Header().Add("WWW-Authenticate", `Basic realm="Give username and password"`)
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte(`{"message": "No basic auth present"}`))
+		return
+	}
+
+	if !isAuthorised(username, password) {
+		w.Header().Add("WWW-Authenticate", `Basic realm="Give username and password"`)
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte(`{"message": "Invalid username or password"}`))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "welcome to golang world!"}`))
+	return
+}
+
 func main() {
-	println("asdasd")
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:4200", "http://localhost:8080/example"},
+	})
+	handler := http.HandlerFunc(greeting)
+	err := http.ListenAndServe(":8080", c.Handler(handler))
+	if err != nil {
+		return
+	}
+	fmt.Println("Starting Server at port :8080")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module AustemServer
 
 go 1.19
+
+require github.com/rs/cors v1.9.0 // indirect
