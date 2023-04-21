57885d82646ba401dc96e4656c467a4797e1cc66
enyad <enindima1@gmail.com>
2023-04-21 22:54:01 +0700

    a

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func formatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d%02d/%02d", year, month, day)
+}
+
 func main() {
-	println("asdasd")
+	router := gin.Default()
+	router.Delims("{[{", "}]}")
+	router.SetFuncMap(template.FuncMap{
+		"formatAsDate": formatAsDate,
+	})
+
+	router.GET("/raw", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "raw.tmpl", gin.H{
+			"now": time.Date(2017, 0o7, 0o1, 0, 0, 0, 0, time.UTC),
+		})
+	})
+
+	err := router.Run(":8080")
+	if err != nil {
+		return
+	}
 }
